c2cb276f04d77a7d8edcb4fcf614a6954e7c39ff
PRIYANK KHAIRWAR <89067679+Priyank-Khairwar@users.noreply.github.com>
2022-02-27 17:25:24 +0530

    Update Querys.sql

--- Querys.sql
+++ Querys.sql
@@ -1,4 +1,4 @@
-#Question1
+/*#Question1*/
 
 /*Query all columns for all American cities in CITY with populations larger than 100,000. The CountryCode for America is USA.
 Input Format
@@ -12,7 +12,26 @@ The CITY table is described as follows:
 | DISTRICT    | VARCHAR2(20) |
 | POPULATION  | NUMBER       |
 ------------------------------
-*/
-Query:
-          (select*from CITY where POPULATION > 100000 and COUNTRYCODE = "USA");
+
+Query:*/
+         (select*from CITY where POPULATION > 100000 and COUNTRYCODE = "USA");
           
+ /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/         
+          
+ /*Question-2*/
+
+/*Query all columns for a city in CITY with the ID 1661.
+Input Format
+The CITY table is described as follows:
+------------------------------
+|   Field     |    Type      |
+------------------------------
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
+------------------------------
+
+Query:*/
+select * from city where id=1661;
